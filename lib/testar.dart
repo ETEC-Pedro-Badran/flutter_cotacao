efd538ddf99e06959f2ff8bcb7352d190b820a28
evaldo@parser.com.br <evaldo@parser.com.br>
2022-11-07 14:36:45 -0300

    Obtendo a cotação através da API Rest.

--- lib/testar.dart
+++ lib/testar.dart
@@ -4,9 +4,19 @@ import 'package:cotacao/service_moeda.dart';
 
 Future<void> main(List<String> args) async {
   var moedasSerializada = await ServiceMoeda().getAlternativas();
-  var moedaList = json.decode(moedasSerializada);
-  var opcao = 'USD-BRL';
-//  print(moedaList['USD-BRL']);
+  var moedaList = jsonDecode(moedasSerializada);
+  //print(moedaList.entries.map((e)=>e.key).toList());
+   
+   
+ // print(moedaList.entries.map((e)=>e.key).toList()[3]);
+  var opcao = moedaList.entries.first.key;
+  //print(opcao);
+  //print(moedaList[opcao]);
 
   var cotacaoSerializada = await ServiceMoeda().getCotacao(opcao);
+  var cotacao = jsonDecode(cotacaoSerializada) as  Map<String, dynamic>;
+  String chave = cotacao.entries.first.key;
+  print(cotacao[chave]['ask']);
+  
+  
 }
